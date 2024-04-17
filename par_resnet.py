4b023ff24e6a7cac59a805d8a44a175d289c8bc6
Felix Brakel <felixbrakel@quicknet.nl>
2024-04-17 17:01:27 +0200

    Move conv layer to macro graph

--- par_resnet.py
+++ par_resnet.py
@@ -5,6 +5,74 @@ import torch.nn as nn
 import numpy as np
 
 
+class CellA(nn.Module):
+    out_channels = 64 + 32 + 32
+
+    def __init__(self, in_channels, scale=1.0):
+        super(CellA, self).__init__()
+        self.scale = scale
+        self.branch_0 = Conv2d(in_channels, 32, 1, stride=1, padding=0, bias=False)
+        self.branch_1 = nn.Sequential(
+            Conv2d(in_channels, 32, 1, stride=1, padding=0, bias=False),
+            Conv2d(32, 32, 3, stride=1, padding=1, bias=False)
+        )
+        self.branch_2 = nn.Sequential(
+            Conv2d(in_channels, 32, 1, stride=1, padding=0, bias=False),
+            Conv2d(32, 48, 3, stride=1, padding=1, bias=False),
+            Conv2d(48, 64, 3, stride=1, padding=1, bias=False)
+        )
+
+    def forward(self, x):
+        x0 = self.branch_0(x)
+        x1 = self.branch_1(x)
+        x2 = self.branch_2(x)
+        x_res = torch.cat((x0, x1, x2), dim=1)
+
+        return x_res
+
+
+class CellB(nn.Module):
+    out_channels = 192 + 192
+
+    def __init__(self, in_channels, scale=1.0):
+        super(CellB, self).__init__()
+        self.scale = scale
+        self.branch_0 = Conv2d(in_channels, 192, 1, stride=1, padding=0, bias=False)
+        self.branch_1 = nn.Sequential(
+            Conv2d(in_channels, 128, 1, stride=1, padding=0, bias=False),
+            Conv2d(128, 160, (1, 7), stride=1, padding=(0, 3), bias=False),
+            Conv2d(160, 192, (7, 1), stride=1, padding=(3, 0), bias=False)
+        )
+
+    def forward(self, x):
+        x0 = self.branch_0(x)
+        x1 = self.branch_1(x)
+        x_res = torch.cat((x0, x1), dim=1)
+
+        return x_res
+
+
+class CellC(nn.Module):
+    out_channels = 192 + 256
+
+    def __init__(self, in_channels, scale=1.0, activation=True):
+        super(CellC, self).__init__()
+        self.scale = scale
+        self.activation = activation
+        self.branch_0 = Conv2d(in_channels, 192, 1, stride=1, padding=0, bias=False)
+        self.branch_1 = nn.Sequential(
+            Conv2d(in_channels, 192, 1, stride=1, padding=0, bias=False),
+            Conv2d(192, 224, (1, 3), stride=1, padding=(0, 1), bias=False),
+            Conv2d(224, 256, (3, 1), stride=1, padding=(1, 0), bias=False)
+        )
+
+    def forward(self, x):
+        x0 = self.branch_0(x)
+        x1 = self.branch_1(x)
+        x_res = torch.cat((x0, x1), dim=1)
+        return x_res
+
+
 class MacroStage(nn.Module):
     def __init__(self, cell, partitions: int, cell_channels: int, stem_scale: int):
         super(MacroStage, self).__init__()
@@ -13,14 +81,16 @@ class MacroStage(nn.Module):
         self.partitions = partitions
         self.cells = nn.ModuleList(cell(cell_channels) for _ in range(self.partitions))
         self.relu = nn.ReLU(inplace=True)
+        self.conv = nn.Conv2d(cell.out_channels * partitions, cell_channels, 1, stride=1, padding=0, bias=True)
         self.stem_scale = stem_scale
 
     def forward(self, x):
         cell_out = []
         for p in range(self.partitions):
             cell_out.append(self.cells[p](x))
-        cell_out = torch.stack(cell_out)
-        return self.relu(torch.sum(cell_out, dim=0) + x * self.stem_scale)
+        cell_out = torch.cat(cell_out, dim=1)
+
+        return self.relu(self.conv(cell_out) + x * self.stem_scale)
 
 
 class ParResNet(nn.Module):
@@ -31,19 +101,19 @@ class ParResNet(nn.Module):
 
         self.stage1 = []
         for _ in range(5):
-            self.stage1.append(MacroStage(Inception_ResNet_A, 2, 320, 0.17))
+            self.stage1.append(MacroStage(CellA, 2, 320, 0.17))
         self.stage1 = nn.Sequential(*self.stage1)
         self.reduction1 = Reduction_A(320, k, l, m, n)
 
         self.stage2 = []
         for _ in range(10):
-            self.stage2.append(MacroStage(Inception_ResNet_B, 2, 1088, 0.1))
+            self.stage2.append(MacroStage(CellB, 2, 1088, 0.1))
         self.stage2 = nn.Sequential(*self.stage2)
         self.reduction2 = Reduction_B(1088)
 
         self.stage3 = []
         for _ in range(5):
-            self.stage3.append(MacroStage(Inception_ResNet_C, 2, 2080, 0.2))
+            self.stage3.append(MacroStage(CellC, 2, 2080, 0.2))
         self.stage3 = nn.Sequential(*self.stage3)
 
         self.conv = Conv2d(2080, 1536, 1, stride=1, padding=0, bias=False)
