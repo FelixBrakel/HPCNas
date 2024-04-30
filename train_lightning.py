f6a8923fe2fefe8b32ee9f71d580dc9f010c6601
Felix Brakel <felixbrakel@quicknet.nl>
2024-04-30 16:46:21 +0200

    Add resnet with grouped conv

--- train_lightning.py
+++ train_lightning.py
@@ -13,22 +13,20 @@ from torchvision.datasets import CIFAR100
 from torchvision import transforms
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from par_resnet import ParResNet
+from grouped_resnet import GroupedResNet
+
 
 # Disgusting globals
 model_dict = {
-    'ParResNet': ParResNet
+    'ParResNet': ParResNet,
+    'GroupedResNet': GroupedResNet
 }
+
+
 # Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
 DATASET_PATH = "./data"
 # Path to the folder where the pretrained models are saved
 CHECKPOINT_PATH = "./saved_models/"
-
-act_fn_by_name = {
-    "tanh": nn.Tanh,
-    "relu": nn.ReLU,
-    "leakyrelu": nn.LeakyReLU,
-    "gelu": nn.GELU
-}
 
 
 def set_seed(seed):
@@ -58,18 +56,16 @@ def setup():
 def create_model(model_name, model_hparams):
     if model_name in model_dict:
         return model_dict[model_name](**model_hparams)
-    else:
-        assert False, f"Unknown model name \"{model_name}\". Available models are: {str(model_dict.keys())}"
 
 
 class CIFARModule(pl.LightningModule):
     def __init__(self, model_name, model_hparams, optimizer_name, optimizer_hparams):
         """
         Inputs:
-            model_name - Name of the model/CNN to run. Used for creating the model (see function below)
+            model_name - Name of the model/CNN to run. Used for creating the model
             model_hparams - Hyperparameters for the model, as dictionary.
             optimizer_name - Name of the optimizer to use. Currently supported: Adam, SGD
-            optimizer_hparams - Hyperparameters for the optimizer, as dictionary. This includes learning rate, weight decay, etc.
+            optimizer_hparams - Hyperparameters for the optimizer, as dictionary.
         """
         super().__init__()
         # Exports the hyperparameters to a YAML file, and create "self.hparams" namespace
@@ -88,7 +84,6 @@ class CIFARModule(pl.LightningModule):
     def configure_optimizers(self):
         # We will support Adam or SGD as optimizers.
         if self.hparams.optimizer_name == "Adam":
-            # AdamW is Adam with a correct implementation of weight decay (see here for details: https://arxiv.org/pdf/1711.05101.pdf)
             optimizer = optim.AdamW(
                 self.parameters(), **self.hparams.optimizer_hparams)
         elif self.hparams.optimizer_name == "SGD":
@@ -98,7 +93,7 @@ class CIFARModule(pl.LightningModule):
 
         # We will reduce the learning rate by 0.1 after 100 and 150 epochs
         scheduler = optim.lr_scheduler.MultiStepLR(
-            optimizer, milestones=[100, 150], gamma=0.1)
+            optimizer, milestones=[15], gamma=0.1)
         return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
@@ -131,8 +126,8 @@ class CIFARModule(pl.LightningModule):
 def train_model(model_name, save_name=None, **kwargs):
     """
     Inputs:
-        model_name - Name of the model you want to run. Is used to look up the class in "model_dict"
-        save_name (optional) - If specified, this name will be used for creating the checkpoint and logging directory.
+        model_name - Name of the model you want to run.
+        save_name (optional) - checkpoint and logging directory.
     """
     if save_name is None:
         save_name = model_name
@@ -143,54 +138,73 @@ def train_model(model_name, save_name=None, **kwargs):
     print("Data mean", DATA_MEANS)
     print("Data std", DATA_STD)
 
-    test_transform = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize(DATA_MEANS, DATA_STD)
-                                         ])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(DATA_MEANS, DATA_STD)
+    ])
+
     # For training, we add some augmentation. Networks are too powerful and would overfit.
-    train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                          transforms.RandomResizedCrop((32,32), scale=(0.8,1.0), ratio=(0.9,1.1)),
-                                          transforms.ToTensor(),
-                                          transforms.Normalize(DATA_MEANS, DATA_STD)
-                                          ])
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomResizedCrop((32,32), scale=(0.8,1.0), ratio=(0.9,1.1)),
+        transforms.ToTensor(),
+        transforms.Normalize(DATA_MEANS, DATA_STD)
+    ])
+
     # Loading the training dataset. We need to split it into a training and validation part
     # We need to do a little trick because the validation set should not use the augmentation.
-    train_dataset = CIFAR100(root=DATASET_PATH, train=True, transform=train_transform, download=True)
-    val_dataset = CIFAR100(root=DATASET_PATH, train=True, transform=test_transform, download=True)
+    train_dataset = CIFAR100(
+        root=DATASET_PATH, train=True, transform=train_transform, download=True
+    )
+    val_dataset = CIFAR100(
+        root=DATASET_PATH, train=True, transform=test_transform, download=True
+    )
     set_seed(42)
     train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
     set_seed(42)
     _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
 
     # Loading the test set
-    test_set = CIFAR100(root=DATASET_PATH, train=False, transform=test_transform, download=True)
+    test_set = CIFAR100(
+        root=DATASET_PATH, train=False, transform=test_transform, download=True
+    )
 
     # We define a set of data loaders that we can use for various purposes later.
-    train_loader = data.DataLoader(train_set, batch_size=64, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
-    val_loader = data.DataLoader(val_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4)
-    test_loader = data.DataLoader(test_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4)
+    train_loader = data.DataLoader(
+        train_set, batch_size=64, shuffle=True, drop_last=True, pin_memory=True, num_workers=4
+    )
+    val_loader = data.DataLoader(
+        val_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4
+    )
+    test_loader = data.DataLoader(
+        test_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4
+    )
 
     # Create a PyTorch Lightning trainer with the generation callback
-    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),                          # Where to save models
-                         accelerator="gpu",                                                                  # We run on a GPU
-                         devices=1,                                                                          # How many GPUs/CPUs we want to use (1 is enough for the notebooks)
-                         max_epochs=180,                                                                     # How many epochs to train for if no patience is set
-                         callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),  # Save the best checkpoint based on the maximum val_acc recorded. Saves only weights and not optimizer
-                                    LearningRateMonitor("epoch")],                                           # Log learning rate every epoch
-                         enable_progress_bar=True,
-                         precision="16-mixed")                                                           # Set to False if you do not want a progress bar
-    trainer.logger._log_graph = True         # If True, we plot the computation graph in tensorboard
-    trainer.logger._default_hp_metric = None # Optional logging argument that we don't need
+    trainer = pl.Trainer(
+        default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),
+        accelerator="gpu",
+        devices=1,
+        max_epochs=20,
+        callbacks=[
+            ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
+            LearningRateMonitor("epoch")
+        ],
+        enable_progress_bar=True,
+    )
+    trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
+    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
 
     # Check whether pretrained model exists. If yes, load it and skip training
     pretrained_filename = os.path.join(CHECKPOINT_PATH, save_name + ".ckpt")
     if os.path.isfile(pretrained_filename):
         print(f"Found pretrained model at {pretrained_filename}, loading...")
-        model = CIFARModule.load_from_checkpoint(pretrained_filename) # Automatically loads the model with the saved hyperparameters
+        model = CIFARModule.load_from_checkpoint(pretrained_filename)
     else:
         pl.seed_everything(42) # To be reproducable
         model = CIFARModule(model_name=model_name, **kwargs)
         trainer.fit(model, train_loader, val_loader)
-        model = CIFARModule.load_from_checkpoint(trainer.checkpoint_callback.best_model_path) # Load best checkpoint after training
+        model = CIFARModule.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
 
     # Test best model on validation and test set
     val_result = trainer.test(model, val_loader, verbose=False)
@@ -203,14 +217,14 @@ def train_model(model_name, save_name=None, **kwargs):
 def main():
     setup()
     model, results = train_model(
-        model_name="ParResNet",
+        model_name="GroupedResNet",
         model_hparams={
             "in_channels": 3,
             "classes": 100,
-            "s0_depth": 1,
-            "s1_depth": 2,
-            "s2_depth": 1,
-            "partitions": 1
+            "s0_depth": 10,
+            "s1_depth": 20,
+            "s2_depth": 10,
+            "groups": 1
         },
         optimizer_name="Adam",
         optimizer_hparams={
