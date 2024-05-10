79cb9f102888d98dc96995d4915df2e5c763f2e2
Felix Brakel <fbrakel@fs0.cm.cluster>
2024-05-10 11:20:46 +0200

    Add some shit

--- train_lightning.py
+++ train_lightning.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import json
-
+import argparse
 import os
 import numpy as np
 import random
@@ -12,7 +12,7 @@ import random
 import torchvision
 from torchvision.datasets import CIFAR100, Imagenette, ImageNet, ImageFolder
 from torchvision import transforms
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint, EarlyStopping
 from par_resnet import ParResNet
 from grouped_resnet import GroupedResNet
 
@@ -21,7 +21,7 @@ from PIL import Image
 # Disgusting globals
 model_dict = {
     'ParResNet': ParResNet,
-    'GroupedResNet': GroupedResNet
+    'GroupedResNet': GroupedResNet,
     'GroupedResNet-ImageNet-100': GroupedResNet
 }
 
@@ -99,7 +99,7 @@ class ResNetModule(pl.LightningModule):
 
         # We will reduce the learning rate by 0.1 after 100 and 150 epochs
         scheduler = optim.lr_scheduler.MultiStepLR(
-            optimizer, milestones=[15], gamma=0.1)
+            optimizer, milestones=[20], gamma=0.1)
         return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
@@ -110,8 +110,8 @@ class ResNetModule(pl.LightningModule):
         acc = (preds.argmax(dim=-1) == labels).float().mean()
 
         # Logs the accuracy per epoch to tensorboard (weighted average over batches)
-        self.log('train_acc', acc, on_step=False, on_epoch=True)
-        self.log('train_loss', loss)
+        self.log('train_acc', acc, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('train_loss', loss, sync_dist=True)
         return loss  # Return tensor to call ".backward" on
 
     def validation_step(self, batch, batch_idx):
@@ -119,17 +119,17 @@ class ResNetModule(pl.LightningModule):
         preds = self.model(imgs).argmax(dim=-1)
         acc = (labels == preds).float().mean()
         # By default logs it per epoch (weighted average over batches)
-        self.log('val_acc', acc)
+        self.log('val_acc', acc, sync_dist=True)
 
     def test_step(self, batch, batch_idx):
         imgs, labels = batch
         preds = self.model(imgs).argmax(dim=-1)
         acc = (labels == preds).float().mean()
         # By default logs it per epoch (weighted average over batches), and returns it afterwards
-        self.log('test_acc', acc)
+        self.log('test_acc', acc, sync_dist=True)
 
 
-def train_model(model_name, save_name=None, **kwargs):
+def train_model(model_name, save_name=None, gpus=1, nodes=2, **kwargs):
     """
     Inputs:
         model_name - Name of the model you want to run.
@@ -138,7 +138,7 @@ def train_model(model_name, save_name=None, **kwargs):
     if save_name is None:
         save_name = model_name
 
-   test_transform = transforms.Compose([
+    test_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
@@ -154,75 +154,75 @@ def train_model(model_name, save_name=None, **kwargs):
 
     # Loading the training dataset. We need to split it into a training and validation part
     # We need to do a little trick because the validation set should not use the augmentation.
-   train_set = ImageFolder(
+    train_set = ImageFolder(
         root=DATASET_PATH + '/train', transform=train_transform
     )
     test_set = ImageFolder(
         root=DATASET_PATH + '/val', transform=test_transform
     )
-    # test_set = Imagenette(
-    #     root=DATASET_PATH, split='test', transform=test_transform, download=True
-    # )
-    # set_seed(42)
     train_set, val_set = torch.utils.data.random_split(train_set, [9/10, 1/10])
-    # set_seed(42)
-    # _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
 
-    # Loading the test set
 
     # We define a set of data loaders that we can use for various purposes later.
     train_loader = data.DataLoader(
-        train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4
+        train_set, batch_size=64, shuffle=True, drop_last=True, pin_memory=True, num_workers=4
     )
     val_loader = data.DataLoader(
-        val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4
+        val_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4
     )
     test_loader = data.DataLoader(
-        test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4
+        test_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4
     )
 
     # Create a PyTorch Lightning trainer with the generation callback
     trainer = pl.Trainer(
         default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),
         accelerator="gpu",
-        devices=4,
-        max_epochs=20,
+        devices="auto",
+	num_nodes=nodes,
+        max_epochs=80,
         callbacks=[
             ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
-            LearningRateMonitor("epoch")
+            LearningRateMonitor("epoch"),
+	    EarlyStopping(
+     	        monitor="val_acc",
+	        min_delta=0.01,
+        	patience=5,
+	        verbose=False,
+        	mode="max")
         ],
-        enable_progress_bar=True,
+        enable_progress_bar=True
     )
     trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
-    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
 
     # Check whether pretrained model exists. If yes, load it and skip training
     pretrained_filename = os.path.join(CHECKPOINT_PATH, save_name + ".ckpt")
     if os.path.isfile(pretrained_filename):
         print(f"Found pretrained model at {pretrained_filename}, loading...")
-        model = CIFARModule.load_from_checkpoint(pretrained_filename)
+        model = ResNetModule.load_from_checkpoint(pretrained_filename)
     else:
-        pl.seed_everything(42) # To be reproducable
-        model = CIFARModule(model_name=model_name, **kwargs)
+        model = ResNetModule(model_name=model_name, **kwargs)
         trainer.fit(model, train_loader, val_loader)
-        model = CIFARModule.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
+        model = ResNetModule.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
 
     # Test best model on validation and test set
-    val_result = trainer.test(model, val_loader, verbose=False)
     test_result = trainer.test(model, test_loader, verbose=False)
-    result = {"test": test_result[0]["test_acc"], "val": val_result[0]["test_acc"]}
 
-    return model, result
+    return model, test_result
 
 
 def main():
     parser = argparse.ArgumentParser(description="Train a model with specified hyperparameters")
     parser.add_argument("--groups", type=int, default=1, help="Number of groups for the model")
+    parser.add_argument("--gpus", type=int, default=1, help="Number of GPUs on a single node")
+    parser.add_argument("--nodes", type=int, default=1, help="Number of nodes")
     args = parser.parse_args()
 
     setup()
     model, results = train_model(
         model_name="GroupedResNet-ImageNet-100",
+	gpus=args.gpus,
+	nodes=args.nodes, 
         model_hparams={
             "in_channels": 3,
             "classes": 100,
